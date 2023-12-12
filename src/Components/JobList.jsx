c20bd6cf3c1cd6be84f39f036ba3ad3b62a6af29
Idzikowski20 <Patryk.idzikowski@interia.pl>
2023-12-12 20:48:58 +0100

    JobCards styling and rensposive for whole web page

--- src/Components/JobList.jsx
+++ src/Components/JobList.jsx
@@ -8,22 +8,30 @@ const JobList = ({ data, filterJobs }) => {
       {data.filter(filterJobs).map((val) => (
         <Link to={`/job/${val.id}`} key={val.id}>
           <div className="template">
-            <div className="offer-image">
-              <img src={val.image} alt="logo company" />
-            </div>
-            <div>
-              <div className="offer-title">
+            <div className="offer-top">
+              <div className="offer-image-title">
+                <img src={val.image} alt="logo company" />
                 <h3>{val.title}</h3>
               </div>
-              <div className="offer-description">
-                <p>{val.description}</p>
-                <p>{val.description}</p>
-                <p>{val.description}</p>
+              <div className="offer-form-price">
+                <div className="offer-location">
+                  <p>📍 {val.location}</p>
+                </div>
+                <div className="offer-price-form-company">
+                  <p>💻 {val.workForm}</p>
+                  <div className="price">
+                    <p>{val.price}</p>
+                    <span> {"ZŁ (B2B) 💸"}</span>
+                  </div>
+                  <p>🏙 {val.company}</p>
+                </div>
               </div>
             </div>
-            <div className="offer-form-price">
-              <p>{val.workForm}</p>
-              <p className="price">{val.price}PLN</p>
+            <div className="offer-description">
+              <p>{val.description}</p>
+              <p>{val.description1}</p>
+              <p>{val.description2}</p>
+              <p>{val.description3}</p>
             </div>
           </div>
         </Link>
